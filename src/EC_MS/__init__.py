6b2e87d39f7146a54a881f945104a70532ecfeda
soren@thomson <scott.soren@gmail.com>
2020-03-06 11:35:16 +0100

    Added metadata to __init__

--- src/EC_MS/__init__.py
+++ src/EC_MS/__init__.py
@@ -7,12 +7,24 @@ For a description of this package, see the included NOTES__EC_MS.text
 @author: Scott
 '''
 
+__version__ = '0.5.0.dev0'
+__title__ = 'EC_MS'
+__description__ = 'Analysis tools for electrochemistry and mass spectrometry and a lot in between'
+__url__ = "https://github.com/ScottSoren/EC_MS"
+__author__ = "Soren B. Scott"
+__email__ = "sbs@spectroinlets.com"
+
+__license = "MIT"
+
+print('\n' + '-'*10 + '  Importing: EC_MS ' + __version__ + ' ' + '-'*10)
+print('from ' + __file__ + '\n\n')
+
 
 from EC_MS.Data_Importing import *
 from EC_MS.Combining import *
 from EC_MS.Plotting import *
 from EC_MS.EC import *
-from EC_MS.Quantification import * #17H08: why wasn't this included?
+from EC_MS.Quantification import *
 from EC_MS.Molecules import *
 from EC_MS.Chips import *
 from EC_MS.Object_Files import *
