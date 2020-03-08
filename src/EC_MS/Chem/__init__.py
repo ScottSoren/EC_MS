40f748c2f4cbe659ccdd26c5c048950cf2a6519f
soren@thomson <scott.soren@gmail.com>
2020-03-08 14:12:16 +0100

    added metadata to and simplified Chem/__init__.py

--- src/EC_MS/Chem/__init__.py
+++ src/EC_MS/Chem/__init__.py
@@ -1,20 +1,13 @@
 
-'''
-Imports Chem package, from within EC_MS package. Want to be able to use
-Chem package both in scripts in EC_MS and in scripts that import EC_MS
-there must be a better way. Need to ask!
-'''
 
-import os 
-
-if os.path.split(os.getcwd())[1] == 'EC_MS':      
-                                #then we're running from inside the EC_MS package
-    from Chem.MolarMasses import *
-    from Chem.PhysCon import *
-    from Chem.Thermochem import *
-else:                           #then we use relative import
-    from EC_MS.Chem.MolarMasses import *
-    from EC_MS.Chem.PhysCon import *
-    from EC_MS.Chem.Thermochem import *
+from EC_MS.Chem.MolarMasses import *
+from EC_MS.Chem.PhysCon import *
+from EC_MS.Chem.Thermochem import *
     
-    
+__version__ = '0.5.0'
+__title__ = 'EC_MS.Chem'
+__description__ = 'Chemistry sub-package to EC_MS. Can calculate molar masses, standard potentials, etc.'
+__url__ = "https://github.com/ScottSoren/EC_MS"
+__author__ = "Soren B. Scott"
+__email__ = "sbs@spectroinlets.com"
+__license = "MIT"
