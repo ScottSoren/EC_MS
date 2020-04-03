94220c6d5524bd46fc4c2ce8168834bc17813582
soren@thomson <scott.soren@gmail.com>
2020-04-03 05:49:20 +0200

    fixed gratuitous string in MolarMasses.py

--- src/EC_MS/Chem/MolarMasses.py
+++ src/EC_MS/Chem/MolarMasses.py
@@ -19,7 +19,7 @@ Zdict = {'H':1,'He':2,
 	# Heavy and Light are so that I can have isotopically labled stuff.
     # + and - are so that charges are collected by break_down
 
-Mdict = {'H':1.008,'He':'4.002',
+Mdict = {'H':1.008,'He':4.002,
 	'Li':6.941,'Be':9.012,'B':10.811,'C':12.011,'N':14.007,'O':15.999,'F':18.998,'Ne':20.180,
 	'Na':22.990,'Mg':24.305,'Al':26.982,'Si':28.086,'P':30.974,'S':32.065,'Cl':35.453,'Ar':39.948,
 	'K':39.098,'Ca':40.078,'Sc':44.956,'Ti':47.867,'V':50.942,'Cr':51.996,'Mn':54.938,'Fe':55.845,'Co':58.933,'Ni':58.693,'Cu':63.546,'Zn':65.380,'Ga':69.723,'Ge':72.640,'As':74.922,'Se':78.96,'Br':79.904,'Kr':83.904,
@@ -49,15 +49,15 @@ def break_down(compound, forgiving=True):
     Breaks a string representing a chemical formula down into constituent parts.
     Things in parentheses are considered one part.
     Any string of a capital letter followed by lower case letters is considered to be
-        an irriducible element. 
+        an irriducible element.
     Any number is considered to quantify the element imediately proceeding it.
     Space is ignored
     Other characters raise a ValueError unless forgiving=True
 
     Example:
-        >>> break_down('CH3(CH2)5CHO') 
+        >>> break_down('CH3(CH2)5CHO')
         >>> {'C':2, 'H':4', 'CH2':5, 'O':1}
-    
+
     This function is called recursively by get_elements.
     """
     parts = {}
@@ -165,7 +165,7 @@ def SimpleForm(*args, **kwargs):
 def get_elements(formula, forgiving=True):
     # Determines number of each element in a complex formula.
     """
-    Given a chemical formula like 'CH3(CH2)5CHO', returns the number of each 
+    Given a chemical formula like 'CH3(CH2)5CHO', returns the number of each
     element, here {'C':7, 'H':14, 'O':1}
     Works recursively if there are parentheses in the formula
     If forgiving=True, considers unidentifiable things to be elements
